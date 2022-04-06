a3be80d42c1f09b355f6a72cfade6a349b55820c
xanxao <101887120+xanxao@users.noreply.github.com>
2022-04-06 19:30:42 -0300

    Delete contact.php

--- contact.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$contactEmail=""; //type default email
-if(empty($contactEmail)){ $contactEmail="contact@".$_SERVER['HTTP_HOST'];}
-
-?>
-
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"></head><body>Contact: <a href="mailto:<?php echo $contactEmail;?>"><?php echo $contactEmail;?></a></body></html>
